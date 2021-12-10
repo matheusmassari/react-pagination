df2f5a7d5443ec9e4b36442738b3a7dac542d21b
Matheus Massari <massari@Matheuss-MacBook-Air.local>
2021-12-10 15:39:47 -0300

    final version

--- src/App.js
+++ src/App.js
@@ -8,9 +8,31 @@ const App = () => {
   const [followers, setFollowers] = useState([]);
 
   useEffect(() => {
-    if(loading) return
-    setFollowers(data[page])
-  }, [loading])
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading, page]);
+
+  const handlePage = (index) => {
+    setPage(index);
+  };
+
+  function nextPage() {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+    });
+  }
+  function prevPage() {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  }
 
   return (
     <main>
@@ -25,7 +47,27 @@ const App = () => {
           })}
         </div>
       </section>
-      {/* {loading ? null : <div className="btn-container"> hello world </div>} */}
+      {!loading && (
+        <div className="btn-container">
+          <button className="prev-btn" onClick={() => prevPage()}>
+            Next
+          </button>
+          {data.map((element, index) => {
+            return (
+              <button
+                className={`page-btn ${index === page ? "active-btn" : null}`}
+                key={index}
+                onClick={() => handlePage(index)}
+              >
+                {index + 1}
+              </button>
+            );
+          })}
+          <button className="next-btn" onClick={() => nextPage()}>
+            Prev
+          </button>
+        </div>
+      )}
     </main>
   );
 };
